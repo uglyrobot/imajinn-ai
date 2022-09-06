24b05a451aea09127f4cf43f12d8f1a2e7e48aa4
Aaron Edwards <aaron@uglyrobot.com>
2022-09-05 23:23:15 -0500

    Fix reformat

--- imajinn-ai.php
+++ imajinn-ai.php
@@ -1,9 +1,16 @@
 <?php
 /**
  * Plugin Name:       Imajinn AI
- * Description:       Generate the perfect images for your blog in seconds with cutting-edge AI. The Imajinn Block brings AI image generation previously only seen on restricted platforms like DALL·E 2 right into the backend of your website so you can create stunning images for any topic with just
- * your imagination. Requires at least: 6.0 Requires PHP:      7.0 Version:           0.1.0-beta-2 Author:            Infinite Uploads Author URI:        https://infiniteuploads.com Plugin URI:        https://infiniteuploads.com/imajinn/ License:           GPL-2.0-or-later License URI:
- * https://www.gnu.org/licenses/gpl-2.0.html Text Domain:       imajinn-ai
+ * Description:       Generate the perfect images for your blog in seconds with cutting-edge AI. The Imajinn Block brings AI image generation previously only seen on restricted platforms like DALL·E 2 right into the backend of your website so you can create stunning images for any topic with just your imagination.
+ * Requires at least: 6.0
+ * Requires PHP:      7.0
+ * Version:           0.1.0-beta-2
+ * Author:            Infinite Uploads
+ * Author URI:        https://infiniteuploads.com
+ * Plugin URI:        https://infiniteuploads.com/imajinn/
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       imajinn-ai
  *
  * @package           imajinn-ai
  *
