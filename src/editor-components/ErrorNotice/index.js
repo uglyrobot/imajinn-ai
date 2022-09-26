14bddda9fd28164f44e9adc861b9be645db9047b
liketodev1 <liketodev1@gmail.com>
2022-09-26 11:56:41 +0400

    notice fixed

--- src/editor-components/ErrorNotice/index.js
+++ src/editor-components/ErrorNotice/index.js
@@ -1,3 +1,7 @@
+import {
+	Notice,
+	__experimentalText as Text,
+} from '@wordpress/components';
 const ErrorNotice = ({ hasError }) => (<Notice status="warning"><Text>{hasError}</Text></Notice>)
 
 export default ErrorNotice
